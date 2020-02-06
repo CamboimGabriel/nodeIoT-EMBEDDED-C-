a42bb4dbe9302797046dcca6de8b94084b358028
Camboinho <gabrielcamboim99@gmail.com>
2020-02-06 17:46:18 -0300

    commit

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/noderest').then(() => {
-    console.log("Connected to mongo db")
-}).catch(e => {
-    console.log("Erro: ", e)
-});
+mongoose
+  .connect(
+    "mongodb://myUserAdmin:Guitarra7762@localhost:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false"
+  )
+  .then(() => {
+    console.log("Connected to mongo db");
+  })
+  .catch(e => {
+    console.log("Erro: ", e);
+  });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
